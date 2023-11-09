9f4c1ced0b2a950cf35f9e7640d4b3b0e821e201
Rafi <tahmidulrafi13@gmail.com>
2023-11-09 18:43:49 +0600

    token problem solved

--- index.js
+++ index.js
@@ -1,17 +1,14 @@
 const express = require('express');
 const app = express();
 const cors = require('cors');
-const jwt= require('jsonwebtoken');
-const cookieParser = require('cookie-parser');
+// const jwt= require('jsonwebtoken');
+// const cookieParser = require('cookie-parser');
 const { MongoClient, ServerApiVersion, ObjectId } = require('mongodb');
 require('dotenv').config()
-app.use(cors({
-    origin: ['http://localhost:5173', 'http://localhost:5174'],
-    credentials: true
-  }));
+app.use(cors());
 app.use(express.json());
+// app.use(cookieParser());
 const port = process.env.PORT || 5000;
-
 
 
 const uri = `mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASS}@cluster0.r9pshpu.mongodb.net/?retryWrites=true&w=majority`;
@@ -25,12 +22,36 @@ const client = new MongoClient(uri, {
   }
 });
 
+//middlewares
+// middlewares 
+// const logger = (req, res, next) =>{
+//     console.log('log: info', req.method, req.url);
+//     next();
+// }
+
+// const verifyToken = (req, res, next) =>{
+//     const token = req?.cookies?.token;
+//     // console.log('token in the middleware', token);
+//     // no token available 
+//     if(!token){
+//         return res.status(401).send({message: 'unauthorized access'})
+//     }
+//     jwt.verify(token, process.env.ACCESS_TOKEN_SECRET, (err, decoded) =>{
+//         if(err){
+//             return res.status(401).send({message: 'unauthorized access'})
+//         }
+//         req.user = decoded;
+//         next();
+//     })
+// }
+
+
 async function run() {
   try {
     // Connect the client to the server	(optional starting in v4.7)
     // await client.connect();
     // Send a ping to confirm a successful connection
-    await client.db("admin").command({ ping: 1 });
+    // await client.db("admin").command({ ping: 1 });
     console.log("Pinged your deployment. You successfully connected to MongoDB!");
   } finally {
     // Ensures that the client will close when you finish/error
@@ -110,22 +131,32 @@ app.get('/submittedAssignment/:id', async(req, res) => {
 
 //Post api
 //post token for auth
-app.post('/jwt', async (req, res) => {
-    const user= req.body;
-    console.log(user);
-    const token= jwt.sign(user, process.env.ACCESS_TOKEN_SECRET, {expiresIn: '1h'})
-    res
-    .cookie('token', token, {
-      httpOnly: true,
-      secure: false
-    })
-    .send({success: true});
-  })
-  app.post('/logout', async (req, res) => {
-    const user = req.body;
-    console.log('logging out', user);
-    res.clearCookie('token', { maxAge: 0 }).send({ success: true })
-})
+// app.post('/jwt', async (req, res) => {
+//     try{
+//         const user= req.body;
+//     console.log(user);
+//     const token= jwt.sign(user, process.env.ACCESS_TOKEN_SECRET, {expiresIn: '1h'})
+//     res
+//     .cookie('token', token, {
+//       httpOnly: true,
+//       secure: false
+//     })
+//     .send({success: true});
+//     }
+//     catch(error){
+//         console.log(error);
+//     }
+//   })
+//   app.post('/logout', async (req, res) => {
+//     try{
+//         const user = req.body;
+//         console.log('logging out', user);
+//         res.clearCookie('token', { maxAge: 0 }).send({ success: true })
+//     }
+//     catch(error){
+//         console.log(error);
+//     }
+// })
 //post assignments
 app.post('/assignments', async (req, res) =>{
     try{
